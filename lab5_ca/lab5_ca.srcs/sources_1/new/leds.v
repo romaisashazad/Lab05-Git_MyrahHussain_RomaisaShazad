f702a10941bf369de5fac0764b86fcbd37a46180
Romaisa Shazad <rb10107@st.habib.edu.pk>
2026-02-19 10:44:46 +0500

    lab 5

---
+++ lab5_ca/lab5_ca.srcs/sources_1/new/leds.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/19/2026 10:20:12 AM
+// Design Name: 
+// Module Name: leds
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module leds(
+
+    );
+endmodule
